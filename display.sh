2c0de461bf90d1f7bb900a0a7a0f7386dc8d9e93
prinzpiuz <prinzpiuz@gmail.com>
2024-07-23 15:18:02 +0530

    fix(display.sh):typo fix

--- display.sh
+++ display.sh
@@ -17,36 +17,36 @@ chosen=$(echo -e $choices | dmenu -i)
                                                                                                                                                                                                                                                                
 # This is used to determine which external display you have connected
 # This may vary between OS. e.g VGA1 instead of VGA-1
-if [ `xrandr | grep VGA-1 | grep -c ' connected '` -eq 1 ]; then
+if [ $(xrandr | grep VGA-1 | grep -c ' connected ') -eq 1 ]; then
         EXTERNAL_OUTPUT="VGA-1"
 fi
-if [ `xrandr | grep DVI-1 | grep -c ' connected '` -eq 1 ]; then
+if [ $(xrandr | grep DVI-1 | grep -c ' connected ') -eq 1 ]; then
         EXTERNAL_OUTPUT="DVI-1"
 fi
-if [ `xrandr | grep HDMI-1 | grep -c ' connected '` -eq 1 ]; then
+if [ $(xrandr | grep HDMI-1 | grep -c ' connected ') -eq 1 ]; then
         EXTERNAL_OUTPUT="HDMI-1"
 fi
-if [ `xrandr | grep HDMI-2 | grep -c ' connected '` -eq 1 ]; then
+if [ $(xrandr | grep HDMI-2 | grep -c ' connected ') -eq 1 ]; then
         EXTERNAL_OUTPUT="HDMI-2"
 fi
-if [ `xrandr | grep HDMI-3 | grep -c ' connected '` -eq 1 ]; then
+if [ $(xrandr | grep HDMI-3 | grep -c ' connected ') -eq 1 ]; then
         EXTERNAL_OUTPUT="HDMI-3"
 fi
-if [ `xrandr | grep DP1 | grep -c ' connected '` -eq 1 ]; then
+if [ $(xrandr | grep DP1 | grep -c ' connected ') -eq 1 ]; then
         EXTERNAL_OUTPUT="DP-1"
 fi
-if [ `xrandr | grep DP-2 | grep -c ' connected '` -eq 1 ]; then
+if [ $(xrandr | grep DP-2 | grep -c ' connected ') -eq 1 ]; then
         EXTERNAL_OUTPUT="DP-2"
 fi
-if [ `xrandr | grep DP-3 | grep -c ' connected '` -eq 1 ]; then
+if [ $(xrandr | grep DP-3 | grep -c ' connected ') -eq 1 ]; then
         EXTERNAL_OUTPUT="DP-3"
 fi
 
 # xrander will run and turn on the display you want, if you have an option for 3 displays, this will need some modifications
 case "$chosen" in
-    external) xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto --primary ;;
-    laptop) xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT --off ;;
-    clone) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT ;;
-    dual) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --right-of $INTERNAL_OUTPUT --primary ;;
+    External) xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto --primary ;;
+    Laptop) xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT --off ;;
+    Clone) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT ;;
+    Dual) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --right-of $INTERNAL_OUTPUT --primary ;;
 
 esac
